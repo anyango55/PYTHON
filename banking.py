a8d62b8ee2c14fdf0a7b0b7ceeee5fa296c4304d
anyango55 <anyangoc55@gmail.com>
2022-07-11 22:28:53 +0300

    Signed-off-by: anyango55 <anyangoc55@gmail.com>

---
+++ banking.py
@@ -0,0 +1,26 @@
+
+
+#Holds details about user
+
+class User():
+    def __init__(self ,name,age,gender):
+        
+        self.name = name
+        self.age = age
+        self.gender = gender
+    
+
+
+
+    def show_details(self):
+        print ("Create personal details")
+        print("")
+        print("Name", name)
+        print("Age", age)
+        print("Gender", gender)
+
+
+
+
+
+        
